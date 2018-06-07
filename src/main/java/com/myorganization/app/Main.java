7e355770d77b0b5c28ace2df44ac72e231fae7af
05kritika <39408273+05kritika@users.noreply.github.com>
2018-06-07 18:11:35 +0530

    Update Main.java

--- src/main/java/com/myorganization/app/Main.java
+++ src/main/java/com/myorganization/app/Main.java
@@ -5,6 +5,9 @@ public class Main {
 
 		System.out.println("Hello, world!");
 		System.out.println("How are you?");
+		System.out.println("How are you?");
+		System.out.println("How are you?");
+		System.out.println("How are you?");
 		
 		
 	}
